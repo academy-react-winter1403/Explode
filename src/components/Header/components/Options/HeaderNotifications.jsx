41512143d0746a4113cc0590508cd1b74015b8dd
Mohammad Hossein <mohammad@gmail.com>
2025-03-17 22:33:16 +0330

    Change Notification

--- src/components/Header/components/Options/HeaderNotifications.jsx
+++ src/components/Header/components/Options/HeaderNotifications.jsx
@@ -1,10 +1,11 @@
 import { Fragment } from 'react'
 import notificationsIcon from '../../../../assets/img/notification.png'
+import IconSet from '../../../shared/IconSet'
 const HeaderNotifications = () => {
     return (
         <Fragment>
             <div className=" flex  items-center justify-center border-[1px] border-[#DCDCDC] w-[48px] h-[48px] rounded-[50%] cursor-pointer relative">
-                <span className=" bg-no-repeat bg-center w-[100%] h-[100%]" style={{ backgroundImage: `url(${notificationsIcon})` }}></span>
+                <IconSet imageAddress={notificationsIcon} firstSize={25} secondSize={25}/>
                 <div className="bg-[#FF5E5E] w-[16px] h-[16px] absolute flex items-center justify-center text-[#fff] rounded-[50%] text-[10px] top-[32px] right-[0px]">2</div>
             </div>
         </Fragment>
