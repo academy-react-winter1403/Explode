a945ed8020fede0fa08afa13df569a6409a0b625
Mohammad Hossein <mohammad@gmail.com>
2025-03-17 19:34:09 +0330

    Active link mode added for menus

--- src/components/Header/components/Menu/Main/Menu.jsx
+++ src/components/Header/components/Menu/Main/Menu.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router";
 const Menu = ({ children, customStyle }) => {
+    const activeLink = "flex relative justify-center after:w-[4px] after:h-[4px] after:bg-[#fff] after:absolute after:bottom-[-5px] after:rounded-[50%]"
     return (
         <ul className={customStyle}>
-            <li><NavLink>خانه</NavLink></li>
-            <li><NavLink>دوره ها</NavLink></li>
-            <li><NavLink>بلاگ ها</NavLink></li>
-            <li><NavLink>درباره ما</NavLink></li>
+            <li><NavLink to={'/'} className={({ isActive }) => isActive ? activeLink : ''}>خانه</NavLink></li>
+            <li><NavLink to={'/course'} className={({ isActive }) => isActive ? activeLink : ''}>دوره ها</NavLink></li>
+            <li><NavLink to={'/blogs'} className={({ isActive }) => isActive ? activeLink : ''}>بلاگ ها</NavLink></li>
+            <li><NavLink to={'/about-us'} className={({ isActive }) => isActive ? activeLink : ''}>درباره ما</NavLink></li>
             {children}
         </ul>
     )
