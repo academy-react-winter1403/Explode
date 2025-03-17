4e33750775c710af879ccbe4b40c7411c3b8bc60
milad <milad.h.b1382@gmail.com>
2025-03-17 16:19:46 +0330

    make custome input component compelete modular

--- src/components/shared/CustomInputField/index.jsx
+++ src/components/shared/CustomInputField/index.jsx
@@ -1,10 +1,13 @@
-const CustomInputField = () => {
+const CustomInputField = ({ label, name, placeholder }) => {
   return (
     <div className="flex flex-col flex-nowrap gap-[8px]">
-      <label className="font-semibold text-[#2F2F2F]">شماره همراه</label>
+      <label htmlFor="name" className="font-semibold text-[#2F2F2F]">
+        {label}
+      </label>
       <input
         type="text"
-        placeholder="شماره همراه خود را وارد کنید"
+        name={name}
+        placeholder={placeholder}
         className="h-[48px] rounded-[24px] border-[1px] border-[#DCDCDC] bg-[#FCFCFC] px-[12px] text-sm font-medium outline-none"
       />
     </div>
