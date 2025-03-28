4123422a6a6cd6960e567edadb0a3521ed45b1e5
Mohammad Hossein <mohammad@gmail.com>
2025-03-28 12:36:46 +0330

    Course list pagination implemented and connected to the API

--- src/pages/Courses/content/course-list/index.jsx
+++ src/pages/Courses/content/course-list/index.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Cards from '../../../../components/shared/cards'
 import CardsSkeleton from '../../../skeleton/cards-skeleton'
-
-const CourseList = ({ loading, courses, setCourses }) => {
+import ReactPaginate from 'react-paginate';
+const CourseList = ({
+    loading,
+    courses,
+    totalCourses,
+    setCurrentPage,
+    perPage,
+    currentPage
+}) => {
+    const pageCount = Math.ceil(totalCourses / perPage)
+    const handlePageClick = (data) => {
+        setCurrentPage(data.selected + 1)
+    }
     return (
         <div className='w-[1031px] '>
             {/* Sorting List */}
@@ -19,7 +30,7 @@ const CourseList = ({ loading, courses, setCourses }) => {
             {/* Course List */}
             <div className='flex justify-between flex-wrap gap-[20px]'>
                 {
-                    loading ? Array(6).fill(0).map((_, index) => (<CardsSkeleton key={index} width={322} height={293} />)) : courses.map((item, index) => (
+                    loading ? Array(perPage).fill(0).map((_, index) => (<CardsSkeleton key={index} width={322} height={293} />)) : courses.map((item, index) => (
                         <Cards
                             isCourse={true}
                             key={index}
@@ -29,11 +40,25 @@ const CourseList = ({ loading, courses, setCourses }) => {
                             courseLevel={item.levelName}
                             courseCategory={item.technologyList.split(',')[0]}
                             price={item.cost}
+                            image={item.tumbImageAddress}
                         />
                     ))
                 }
             </div>
 
+            {/* Pagination */}
+            <div className='flex justify-center mt-[50px]'>
+                {<ReactPaginate
+                    pageCount={pageCount}
+                    breakLabel={null}
+                    activeLinkClassName={'bg-primary text-[#FEFDFF] p-[11px_18px] rounded-[8px]'}
+                    className={'flex text-thirdly flex-row-reverse gap-[20px] h-[48px] bg-[#EFEFEF] items-center rounded-[16px] p-[0_20px] text-[16px] font-[700]'}
+                    previousLabel={currentPage > 1 ? <span className={'w-[24px] flex h-[24px] bg-center bg-contain bg-no-repeat cursor-pointer'} style={{ backgroundImage: `url('/src/assets/icons/leftArrow.svg')` }}></span> : null}
+                    nextLabel={currentPage < pageCount ? <span className={'w-[24px] flex h-[24px] bg-center bg-contain bg-no-repeat cursor-pointer'} style={{ backgroundImage: `url('/src/assets/icons/rightArrow.svg')` }}></span> : null}
+                    pageClassName={'cursor-pointer select-none'}
+                    onPageChange={handlePageClick}
+                />}
+            </div>
 
         </div>
     )
--- src/pages/Courses/content/index.jsx
+++ src/pages/Courses/content/index.jsx
@@ -6,13 +6,19 @@ import { getCoursesWithPagination } from '../../../core/services/api/courses'
 const Content = () => {
     const [courses, setCourses] = useState([])
     const [loading, setLoading] = useState(true)
-    const getAllCourses = async () => {
+    const [totalCourses, setTotalCourses] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
+    let perPage = 12
+    const getAllCourses = async (page, perPage) => {
         const params = {
-            RowsOfPage: 6
+            RowsOfPage: perPage,
+            PageNumber: page
         }
         try {
-            const { courseFilterDtos: result } = await getCoursesWithPagination(params)
+            setLoading(true)
+            const { courseFilterDtos: result, totalCount: coursesCount } = await getCoursesWithPagination(params)
             setCourses(result)
+            setTotalCourses(coursesCount)
             setLoading(false)
         }
         catch {
@@ -21,15 +27,21 @@ const Content = () => {
     }
 
     useEffect(() => {
-        getAllCourses()
-    }, [])
+        getAllCourses(currentPage, perPage)
+    }, [currentPage])
 
     return (
         <section className='max-w-[1360px] flex justify-between items-start m-[0_auto] mb-[80px] max-[1460px]:p-[0_16px]'>
             <CourseFilter />
-            <CourseList loading={loading} courses={courses} setCourses={setCourses} />
+            <CourseList
+                loading={loading}
+                courses={courses}
+                totalCourses={totalCourses}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                perPage={perPage}
+            />
         </section>
     )
 }
-
 export default Content
