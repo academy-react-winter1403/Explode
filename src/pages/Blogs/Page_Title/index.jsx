61375a50c9032c3563977340bb4dd24d20d650af
dorsa <dori@gmail.com>
2025-03-30 10:21:47 -0700

    update blog

---
+++ src/pages/Blogs/Page_Title/index.jsx
@@ -0,0 +1,11 @@
+import PageTitle from './../../../components/shared/page-titile/index';
+const Page_Title = () => {
+  return (
+    <PageTitle
+      title={'اطلاعات بیشتر درک بهتر'}
+      line1={`ما در بلاگ ها اطلاعات شما رو نسبت به`}
+      line2={'تکنولوژی که یاد می گیرید بیشتر میکنیم'}
+    />
+  );
+};
+export default Page_Title;
--- src/pages/Blogs/index.jsx
+++ src/pages/Blogs/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import Content from '../Courses/content';
+import Page_Title from './page_title';
 
-const Blogs = () => {
-    return (
-        <div>Blogs</div>
-    )
-}
-
-export default Blogs
+const Courses = () => {
+  return (
+    <Fragment>
+      <Page_Title />
+      <Content />
+    </Fragment>
+  );
+};
+export default Courses;
