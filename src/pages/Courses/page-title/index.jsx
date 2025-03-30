8471c0041b6a11fb000d320c2b08763d909d4765
Mohammad Hossein <mohammad@gmail.com>
2025-03-30 12:54:23 +0330

    Updated Page Title Structure

--- src/pages/Courses/page-title/index.jsx
+++ src/pages/Courses/page-title/index.jsx
@@ -1,15 +1,11 @@
-const PageTitle = () => {
+import PageTitle from './../../../components/shared/page-titile/index';
+const Page_Title = () => {
     return (
-        <section className='max-w-[1360px] m-[80px_auto] text-center '>
-            <h1 className="font-[700] text-thirdly text-[40px] mb-[30px]">
-                شروع ماجراجویی جدید
-            </h1>
-            <p className="font-[500] text-[18px] text-[#707070]">
-                یک شروع قوی برای یادگیری یک مسئله <br />
-                جدید میتونه تو پیشترفت کمک کنه
-            </p>
-        </section>
+        <PageTitle
+            title={'شروع ماجراجویی جدید'}
+            line1={`یک شروع قوی برای یادگیری یک مسئله`}
+            line2={'جدید میتونه تو پیشترفت کمک کنه'}
+        />
     )
 }
-
-export default PageTitle
+export default Page_Title
