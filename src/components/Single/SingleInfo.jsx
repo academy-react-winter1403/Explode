fd7e0249bd8cd8b5bd7a7c6e220cae85e5e375b8
Mohammad Hossein <mohammad@gmail.com>
2025-04-17 12:34:18 +0330

    Updated

--- src/components/Single/SingleInfo.jsx
+++ src/components/Single/SingleInfo.jsx
@@ -1,35 +1,42 @@
-import React from 'react';
+import React from 'react'
 import { ValidURL } from './../../utils/ValidUrl';
 import SingleInfoTable from './SingleInfoTable';
 import SingleInfoOverviewBar from './SingleInfoOverviewBar';
 import SingleInfoFooter from './SingleInfoFooter';
 
 const SingleInfo = ({ detail, blogSingle, courseSingle }) => {
-  const imageAddress =
-    courseSingle && ValidURL(detail.imageAddress)
-      ? detail.imageAddress
-      : blogSingle && ValidURL(detail.currentImageAddressTumb)
-        ? detail.currentImageAddressTumb
-        : false;
-  return (
-    <div className="justift-between flex items-start gap-[10px]">
-      {/* Course Image */}
-      <div className="flex h-[424px] w-[642px] items-center justify-center overflow-hidden">
-        <img
-          className="rounded-[32px]"
-          src={
-            imageAddress ? imageAddress : '/src/assets/img/not-set-image.jpg'
-          }
-          alt={detail.title}
-        />
-      </div>
+    // : blogSingle && ValidURL(detail.currentImageAddressTumb) ? detail.currentImageAddressTumb : false
+    const imageAddress = courseSingle && ValidURL(detail.imageAddress) && detail.imageAddress
+    return (
+        <div className='flex gap-[20px] justify-between items-center max-[1150px]:flex-col'>
+            {/* Single Page Image */}
+            <div className='w-[642px] h-[auto] max-[1150px]:w-[100%] rounded-[32px] flex items-center max-[1150px]:items-start justify-center overflow-hidden max-[1150px]:order-1'>
+                <img className={`rounded-[32px] ${imageAddress ? '' : 'h-[100px]'}`} src={imageAddress ? imageAddress : '/src/assets/img/not-set-image.jpg'} alt={detail?.title} />
+            </div>
 
-      {/* Course Info */}
-      <div className="w-[642px]">
-        <h2 className="text-[32px] font-[700]">{detail.title}</h2>
-      </div>
-    </div>
-  );
-};
+            {/* Single Page Info*/}
+            <div className='w-[710px] max-[1150px]:w-[100%]'>
+                <h2 className='font-[700] text-[32px] mb-[15px]'>{detail?.title}</h2>
+                <SingleInfoTable
+                    detail={detail}
+                    courseSingle={courseSingle}
+                    blogSingle={blogSingle}
+                />
+                <SingleInfoOverviewBar
+                    rating={courseSingle ? detail.currentRate : 4}
+                    commentCount={courseSingle ? detail.commentCount : 126}
+                    courseCost={courseSingle && detail.cost}
+                    courseSingle={courseSingle}
+                />
+                <SingleInfoFooter
+                    courseSingle={courseSingle}
+                    courseCost={courseSingle && detail.cost}
+                />
+            </div>
 
-export default SingleInfo;
+
+        </div>
+    )
+}
+
+export default SingleInfo
