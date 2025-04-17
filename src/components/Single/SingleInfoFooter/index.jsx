449ee0894de2ffc9c7a683fbb54669f052ba2612
Mohammad Hossein <mohammad@gmail.com>
2025-04-17 12:12:17 +0330

    Update

--- src/components/Single/SingleInfoFooter/index.jsx
+++ src/components/Single/SingleInfoFooter/index.jsx
@@ -3,7 +3,8 @@ import IconSet from './../../shared/IconSet/index';
 
 const SingleInfoFooter = ({ courseSingle, courseCost = 0 }) => {
     return (
-        <div className={`flex ${courseSingle ? 'max-[710px]:justify-center': 'max-[746px]:justify-center'}  max-[710px]:w-[100%] gap-[10px] items-center justify-between mt-[20px]`}>
+        <div className={`flex ${courseSingle ? 'max-[710px]:justify-center' : 'max-[746px]:justify-center'}  max-[710px]:w-[100%] gap-[10px] items-center justify-between mt-[20px]`}>
+            {/* Reserve / Copy Page Link*/}
             <div>
                 {
                     courseSingle ?
@@ -15,14 +16,19 @@ const SingleInfoFooter = ({ courseSingle, courseCost = 0 }) => {
                         </span>
                 }
             </div>
+
+            {/* Add To Favorite */}
             <div className='flex p-[13.5px_44px]  items-center cursor-pointer gap-[10px] bg-thirdly text-[#fff] cursor-pointer font-[500] text-[15px] rounded-[48px]'>
                 <IconSet imageAddress={'/src/assets/icons/archive.svg'} /> اضافه به لیست مورد علاقه
             </div>
+
+            {/* Like / Dislike */}
             <div className='flex items-center gap-[10px]'>
                 <span className='w-[56px] h-[56px] border-[1px] border-[#DCDCDC] cursor-pointer rounded-full flex items-center justify-center'><IconSet imageAddress={'/src/assets/icons/like.svg'} /></span>
                 <span className='w-[56px] h-[56px] border-[1px] border-[#DCDCDC] cursor-pointer rounded-full flex items-center justify-center'><IconSet imageAddress={'/src/assets/icons/dislike.svg'} /></span>
             </div>
 
+            {/* Reserve (Responsive) */}
             {
                 courseSingle &&
                 <div className='p-[10px_10px] shadow-[0_-3px_20px_#e2e2e2] fixed max-[710px]:flex  hidden z-800 bottom-0 right-0 flex items-center h-[70px] w-[100%] bg-[#fff] justify-between'>
